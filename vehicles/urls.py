eced2fc14a1595569919817dc5bb42212b038fd7
Maximiliano Garcia <maxiadrigar@gmail.com>
2025-07-08 21:43:21 -0300

    Correccion de urls

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -6,7 +6,7 @@ app_name = 'vehicles'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('vehicles', views.vehicle_list, name='vehicle_list'),
+    path('vehicles/', views.vehicle_list, name='vehicle_list'),
     path('vehicle/<int:pk>/', views.vehicle_detail, name='vehicle_detail'),
     path('type/<int:type_id>/', views.vehicle_type_list, name='vehicle_type_list'),
     path('modelos/<str:brand_name>/', BrandVehicleListView.as_view(), name='brand_list'),
