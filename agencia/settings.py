e29b4967b86a1c25b2e3bc708644ad1187aef1fe
Maximiliano Garcia <maxiadrigar@gmail.com>
2025-07-12 18:42:48 -0300

    Importacion de la libreria decouple

--- agencia/settings.py
+++ agencia/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
